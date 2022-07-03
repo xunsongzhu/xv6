ab6fa0c34cd4f44ea83659c5838548c2d49f86cc
anyushi <434624198@qq.com>
2022-07-03 15:58:01 +0900

    pingpong.c and primes.c

--- user/pingpong.c
+++ user/pingpong.c
@@ -10,46 +10,48 @@
  */
 
 int main(int argc, char *argv[]) {
-    int pid;
-    int fd0[2];
-    int fd1[2];
-    char *byte = "p";
-    if (pipe(fd0) == -1) {
-        fprintf(2, "create pipe error");
-    }
+	int pid;
+	// fd[1]端写入，fd[0]端读出
+	// child process read fd0[0], and write fd1[1]
+	// parent process read fd1[0], and write fd0[1]
+	int fd0[2];
+	int fd1[2];
 
-    if (pipe(fd1) == -1) {
-        fprintf(2, "create pipe error");
-    }
+	char *byte = "p";
+	if (pipe(fd0) == -1) {
+	  fprintf(2, "create pipe error");
+	}
 
-    pid = fork();
-    if (pid < 0) {
-        fprintf(2, "fork error");
-        // fd[0] → r； fd[1] → w
-    } else if (pid == 0) {
-        // child process
-        close(fd0[1]);
-        close(fd1[0]);
-        if (read(fd0[0], byte, 1) != 1) {
-            fprintf(2, "read error\n");
-        }
-        fprintf(1, "%d: received ping\n", getpid());
-        if (write(fd1[1], byte, 1) != 1) {
-            fprintf(2, "write error\n");
-        }
-        exit(0);
-    } else {
-        // parent process
-        close(fd0[0]);
-        close(fd1[1]);
-        if (write(fd0[1], byte, 1) != 1) {
-            fprintf(2, "write error\n");
-        }
-        if (read(fd1[0], byte, 1) != 1) {
-            fprintf(2, "read error\n");
-        }
-        fprintf(1, "%d: received pong\n", getpid());
-        exit(0);
-    }
-    exit(0);
+	if (pipe(fd1) == -1) {
+	  fprintf(2, "create pipe error");
+	}
+
+	pid = fork();
+	if (pid < 0) {
+	  fprintf(2, "fork error");
+	} else if (pid == 0) {
+		// child process
+	  close(fd0[1]);
+	  close(fd1[0]);
+	  if (read(fd0[0], byte, 1) == -1) {
+		fprintf(2, "read error\n");
+	  }
+	  fprintf(1, "%d: received ping\n", getpid());
+	  if (write(fd1[1], byte, 1) == -1) {
+		fprintf(2, "write error\n");
+	  }
+	  exit(0);
+	} else {
+	  // parent process
+	  close(fd0[0]);
+	  close(fd1[1]);
+	  if (write(fd0[1], byte, 1) == -1) {
+		fprintf(2, "write error\n");
+	  }
+	  if (read(fd1[0], byte, 1) == -1) {
+		fprintf(2, "read error\n");
+	  }
+	  fprintf(1, "%d: received pong\n", getpid());
+	}
+  exit(0);
 }
